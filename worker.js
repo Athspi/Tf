fd8d51434301ffdbb10f2b05cf5b27228dc22fe5
ATHSPI <190251249+Athspi@users.noreply.github.com>
2026-08-31 22:14:02 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@
 import * as bitcoin from 'bitcoinjs-lib';
 import * as bip39 from 'bip39';
 import { BIP32Factory } from 'bip32';
-import * as ecc from 'tiny-secp256k1';
+import * as ecc from 'tiny-secp256k1/js';
 
 const bip32 = BIP32Factory(ecc);
 const NETWORK = bitcoin.networks.bitcoin;
